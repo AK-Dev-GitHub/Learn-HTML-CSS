44ac044275d2aebae154619560cb04eea27775a4
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-07-27 22:53:41 +0900

    Add: ハンバーガメニュー開閉 TODO:Jqueryの動作不備あり。

---
+++ Udemy/WEBC/ハンバーガーメニュー開閉/humberger.js
@@ -0,0 +1,11 @@
+// ハンバーガー
+$(function () {
+  $('.headerC').click(function () {
+    $('.headerBack').slideToggle();
+  });
+});
+$(function () {
+  $('#tojiru').click(function () {
+    $('.headerBack').slideToggle();
+  });
+});
